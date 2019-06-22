e370f189564edc25d1b0cb21c4c1d8b6c3543150
ianinakachan <ianinakachan@gmail.com>
2019-06-22 21:58:23 +0300

    hw2 pageContentTest steps 6-9 added

--- src/test/java/hw2/ex1/Exercise1.java
+++ src/test/java/hw2/ex1/Exercise1.java
@@ -29,23 +29,66 @@ public class Exercise1 extends BaseTestHw2 {
         // Step #5 Assert Browser title
         browserTitleCheck("Home Page");
 
-        //Step #6 Assert that there are 4 items on the header section are displayed and they have proper texts
+        /*Step #6
+         Assert that there are 4 items on the header section are displayed
+         and they have proper texts
+         */
         pageContentTest();
-
-
     }
 
     private void pageContentTest() {
 
+        //Step #6 Assert that there are 4 items on the header section are displayed...
+
         List<String> expectedNavBarItemsTexts = Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS");
-        List<WebElement> actualNavBarItems = driver.findElements(By.xpath("//ul[@class='uui-navigation nav navbar-nav m-l8']/li"));
+        List<WebElement> actualNavBarItems = driver.findElements(By.cssSelector(".nav>li>a"));
         assertEquals(actualNavBarItems.size(), 4);
 
-        //List<String> actualNavBarItemsTexts = actualNavBarItems.stream().map(WebElement::getText).collect(Collectors.toList());
+        //Step #6 ...and they have proper texts
         SoftAssert softAssert = new SoftAssert();
+
         for (int i = 0; i < 4; i++) {
             softAssert.assertEquals(actualNavBarItems.get(i).getText(), expectedNavBarItemsTexts.get(i));
         }
+
+        //Step #7 Assert that there are 4 images on the Index Page...
+        List<WebElement> images = driver.findElements(By.cssSelector(".benefit-icon"));
+        assertEquals(images.size(), 4);
+
+        //Step #7 ...and they are displayed
+        for (int i = 0; i < 4; i++) {
+            softAssert.assertTrue(images.get(i).isDisplayed());
+        }
+
+        //Step #8 Assert that there are 4 texts on the Index Page under icons...
+        List<WebElement> actualImageTexts = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(actualImageTexts.size(), 4);
+
+        //Step #8 ...and they have proper text
+        List<String> expectedImageTexts = Arrays.asList(
+                "To include good practices\nand ideas from successful\nEPAM project",
+                "To be flexible and\ncustomizable",
+                "To be multiplatform",
+                "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
+
+        for (int i = 0; i < 4; i++) {
+            softAssert.assertTrue(actualImageTexts.get(i).isDisplayed());
+            softAssert.assertEquals(actualImageTexts.get(i).getText(), expectedImageTexts.get(i));
+        }
+
+        // Step #9 Assert a text of the main headers
+        List<String> expectedMainHeadersTexts = Arrays.asList(
+                "EPAM framework Wishes…",
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
+                        "                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud\n" +
+                        "                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in\n" +
+                        "                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        );
+
+        List<WebElement> actualMainHeadersTexts = driver.findElements(By.cssSelector(".benefit-icon"));
+
+
+
         softAssert.assertAll();
     }
 
