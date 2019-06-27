5214898d4c9262b4f70c2ca9919a3cab85af89f9
ianinakachan <ianinakachan@gmail.com>
2019-06-27 21:11:15 +0300

    hw3 fixed unused method

--- src/test/java/hw3/BaseTestHw3.java
+++ src/test/java/hw3/BaseTestHw3.java
@@ -5,14 +5,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 
 public class BaseTestHw3 {
 
     protected WebDriver driver;
-    protected SoftAssert softAssert;
     protected String url;
 
     @BeforeSuite
@@ -20,7 +18,7 @@ public class BaseTestHw3 {
         WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeTest
+    @BeforeClass
     public void setUp() throws IOException {
         driver = new ChromeDriver();
         url = FileUtils.readPropertiesFromFile("src/test/resources/properties/url.properties").getProperty("url.link");
@@ -28,61 +26,9 @@ public class BaseTestHw3 {
         driver.manage().window().maximize();
     }
 
-    @BeforeMethod
-    public void initSoftAssert() {
-        softAssert = new SoftAssert();
-    }
-
     @AfterClass
-    public void teatDown() {
+    public void tearDown() {
         driver.close();
     }
 
-/*    protected void siteOpeningCheck(String url) {
-        assertEquals(driver.getCurrentUrl(), url);
-    }
-
-    protected void browserTitleCheck(String browserTitle) {
-        assertEquals(driver.getTitle(), browserTitle);
-    }
-
-    protected void loginCheck() {
-        assertEquals(driver.findElement(By.id("user-name")).getText(), userName.toUpperCase());
-    }
-
-    protected void checkElementIsDisplayed(WebElement webElement) {
-        softAssert.assertTrue(webElement.isDisplayed());
-    }
-
-    protected void checkElementsAreDisplayed(List<WebElement> webElements) {
-        for (WebElement element : webElements) {
-            checkElementIsDisplayed(element);
-        }
-    }
-
-    protected void compareLists(List<WebElement> actual, List<String> expected) {
-        assertEquals(actual.size(), expected.size());
-        // TODO It is better to use i < actual.size() - FIXED
-        for (int i = 0; i < actual.size(); ++i) {
-            softAssert.assertEquals(actual.get(i).getText(), expected.get(i));
-        }
-    }
-
-    protected void checkCount(List<WebElement> actual, int expectedCount) {
-        assertEquals(actual.size(), expectedCount);
-    }
-
-    protected void selectElement(WebElement element) {
-        element.click();
-        assertTrue(element.isSelected());
-    }
-
-    protected void deselectElement(WebElement element) {
-        element.click();
-        assertFalse(element.isSelected());
-    }
-
-    protected String getLastLogRecord() {
-        return driver.findElement(By.cssSelector(".panel-body-list.logs > li")).getText();
-    }*/
 }
